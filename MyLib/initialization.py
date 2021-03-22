c3eedcec10e603d12c34e862fa07c3510cb3112d
Hades-Z <310240303@qq.com>
2021-03-22 23:29:44 +0800

    存档

---
+++ MyLib/initialization.py
@@ -0,0 +1,5 @@
+#   Pandas DataFrom 初始化
+def Init():
+    # 设置全部行输出
+    from IPython.core.interactiveshell import InteractiveShell  # 加载相关包
+    InteractiveShell.ast_node_interactivity = "all"  # 执行全部行输出命令
